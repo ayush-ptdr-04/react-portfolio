a07c159b2743ea73496eeaefc92da1a88ea90ef2
aayush patidar <ap6887980@gmail.com>
2024-11-09 20:59:01 +0530

    this is my portfolio website, now not completed but , i will do some changes in few days

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { FaJava } from "react-icons/fa"; // Java icon from FontAwesome
+import { GiGearHammer } from "react-icons/gi";
+import { animate, motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <h1 className="my-20 text-center text-4xl">Technologies</h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-8">
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaHtml5 className="text-7xl text-[#E34F26]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaCss3Alt className="text-7xl text-[#1572B6]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiJavascript className="text-7xl text-[#F7DF1E]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaNodeJs className="text-7xl text-[#68A063]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiExpress className="text-7xl text-[#11111]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMongodb className="text-7xl text-[#47A248]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMysql className="text-7xl text-[#00618A]" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaJava className="text-7xl text-[#007396]" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4">
+          <GiGearHammer className=" text-7xl text-[#FF5733]" />{" "}
+          {/* Gear Icon for DSA */}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologies;
